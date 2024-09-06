5dc8003ea80694ba914f3d58cb4b6c52bda5aeeb
Alexandre Negrel <alexandre@negrel.dev>
2024-09-07 00:25:09 +0200

    add string.contains

--- src/bin/allelua/lua/string.rs
+++ src/bin/allelua/lua/string.rs
@@ -1,26 +1,10 @@
 use mlua::{chunk, Lua};
 
-#[derive(Clone, mlua::FromLua)]
-pub struct LuaStringModule;
-
-impl mlua::UserData for LuaStringModule {
-    fn add_fields<'lua, F: mlua::UserDataFields<'lua, Self>>(#[allow(unused)] fields: &mut F) {
-        for i in 0..100_000 {
-            fields.add_field(i.to_string(), i)
-        }
-    }
-
-    fn add_methods<'lua, M: mlua::UserDataMethods<'lua, Self>>(#[allow(unused)] methods: &mut M) {
-        let tostr = stringify!(LuaStringModule).replace("Lua", "");
-        methods.add_meta_method(mlua::MetaMethod::ToString, move |_, _, ()| {
-            Ok(tostr.to_owned())
-        });
-
-        methods.add_function("noop", |_, _v: mlua::Value| Ok(()))
-    }
-}
-
 pub fn load_string(lua: &'static Lua) -> mlua::Result<()> {
+    // TODO: support mlua::String instead of String which are UTF-8 only.
+    let contains =
+        lua.create_function(|_lua, (str1, str2): (String, String)| Ok(str1.contains(&str2)))?;
+
     lua.load(chunk! {
         local string = require("string");
         local M = string
@@ -35,6 +19,8 @@ pub fn load_string(lua: &'static Lua) -> mlua::Result<()> {
         M.has_suffix = function(str, suffix)
             return string.slice(str, -#suffix) == suffix
         end
+
+        M.contains = $contains
     })
     .eval::<()>()
 }
