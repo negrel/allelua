372d6fe035796fcf97b9da8a4d0077c0feae5bd0
Alexandre Negrel <alexandre@negrel.dev>
2024-09-23 15:19:23 +0200

    add clone example

---
+++ examples/clone.lua
@@ -0,0 +1,25 @@
+local os = require("os")
+local table = require("table")
+local byte = require("byte")
+
+local t = { 1, 2 }
+table.push(t, t)
+
+setmetatable(t, {
+	__clone = function(t, opts)
+		local result = {}
+		opts.replace[t] = result
+
+		for k, v in pairs(t) do
+			result[clone(k, opts)] = clone(v, opts)
+		end
+
+		setmetatable(result, getmetatable(t))
+		return result
+	end
+})
+
+print(t, clone(t))
+
+local buf = byte.Buffer.fromstring("foo")
+print(type(buf), buf, clone(buf))
