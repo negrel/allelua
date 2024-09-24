a93a0023bb6c5da5e90067d42943b3950e66522b
Alexandre Negrel <alexandre@negrel.dev>
2024-09-24 11:25:24 +0200

    update string metatable

--- src/bin/allelua/lua/string.rs
+++ src/bin/allelua/lua/string.rs
@@ -5,22 +5,31 @@ pub fn load_string(lua: Lua) -> mlua::Result<()> {
     let contains =
         lua.create_function(|_lua, (str1, str2): (String, String)| Ok(str1.contains(&str2)))?;
 
-    lua.load(chunk! {
-        local string = require("string");
-        local M = string
+    let string_mt = lua
+        .load(chunk! {
+            local string = require("string");
+            local M = string
 
-        M.slice = M.sub
-        M.sub = nil
+            M.slice = M.sub
+            M.sub = nil
 
-        M.has_prefix = function(str, prefix)
-            return string.slice(str, 0, #prefix) == prefix
-        end
+            M.has_prefix = function(str, prefix)
+                return string.slice(str, 0, #prefix) == prefix
+            end
 
-        M.has_suffix = function(str, suffix)
-            return string.slice(str, -#suffix) == suffix
-        end
+            M.has_suffix = function(str, suffix)
+                return string.slice(str, -#suffix) == suffix
+            end
 
-        M.contains = $contains
-    })
-    .eval::<()>()
+            M.contains = $contains
+
+            return {
+                __index = M
+            }
+        })
+        .eval::<mlua::Table>()?;
+
+    lua.set_type_metatable::<mlua::String>(Some(string_mt));
+
+    Ok(())
 }
