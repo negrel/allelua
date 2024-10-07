c302cb5b18e5dd8b6b52ea9a22327b377446dbfa
Alexandre Negrel <alexandre@negrel.dev>
2024-10-07 12:50:43 +0200

    add file read_line benchmark

---
+++ tests/lua/os/file_bench.lua
@@ -0,0 +1,17 @@
+local os = require("os")
+local package = require("package")
+local path = require("path")
+local t = require("test")
+
+local parent_dir = path.parent(package.meta.path)
+local file_txt = path.join(parent_dir, "testdata/file.txt")
+
+t.bench("read file line by line", function(b)
+	for _i = 1, b.n do
+		local f = os.File.open(file_txt, "r")
+		while true do
+			local line = f:read_line()
+			if not line then break end
+		end
+	end
+end)
