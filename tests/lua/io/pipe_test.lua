0ecbb63bb667258075b950a3dcd3069ad41608a2
Alexandre Negrel <alexandre@negrel.dev>
2024-10-23 23:57:46 +0200

    fix tests relying on global go function

--- tests/lua/io/pipe_test.lua
+++ tests/lua/io/pipe_test.lua
@@ -7,21 +7,22 @@ t.test("single write in io.PipeWriter blocks until data is read", function()
 
 	local reader_done = false
 
-	-- Reader
-	go(function()
-		local buf = string.buffer.new(1024)
-		reader:read(buf)
-		assert(buf:tostring() == "Hello world!")
-		reader_done = true
-	end)
+	coroutine.nursery(function(go)
+		-- Reader
+		go(function()
+			local buf = string.buffer.new(1024)
+			reader:read(buf)
+			assert(buf:tostring() == "Hello world!")
+			reader_done = true
+		end)
 
-	-- Writer
-	do
-		local buf = string.buffer.new()
-		buf:put("Hello world!")
-		writer:write(buf)
-	end
-	coroutine.yield()
+		-- Writer
+		do
+			local buf = string.buffer.new()
+			buf:put("Hello world!")
+			writer:write(buf)
+		end
+	end)
 	assert(reader_done)
 end)
 
@@ -32,58 +33,67 @@ t.test(
 
 		local reader_done = false
 
-		-- Reader
-		go(function()
-			local buf = string.buffer.new()
-			for i = 1, 3 do
-				reader:read(buf, 1)
+		coroutine.nursery(function(go)
+			-- Reader
+			go(function()
+				local buf = string.buffer.new()
+				for i = 1, 3 do
+					reader:read(buf, 1)
+				end
+				assert(buf:tostring() == "foo")
+				reader_done = true
+			end)
+
+			-- Writer
+			do
+				local buf = string.buffer.new()
+				buf:put("foo")
+				writer:write(buf)
+				assert(tostring(buf) == "")
 			end
-			assert(buf:tostring() == "foo")
-			reader_done = true
 		end)
-
-		-- Writer
-		do
-			local buf = string.buffer.new()
-			buf:put("foo")
-			writer:write(buf)
-			assert(tostring(buf) == "")
-		end
-
-		coroutine.yield()
 		assert(reader_done)
 	end
 )
 
 t.test("read returns error on closed pipe", function()
 	local writer, reader = io.pipe()
-	go(writer.close, writer)
 
-	local ok, err = pcall(reader.read, reader, string.buffer.new())
-	assert(not ok and err:is(io.errors.closed))
+	coroutine.nursery(function(go)
+		go(writer.close, writer)
+
+		local ok, err = pcall(reader.read, reader, string.buffer.new())
+		assert(not ok and err:is(io.errors.closed))
+	end)
 end)
 
 t.test("write returns error on closed pipe", function()
 	local writer = io.pipe()
-	go(writer.close, writer)
 
-	local buf = string.buffer.new()
-	buf:put("foo")
-	local ok, err = pcall(writer.write, writer, buf)
-	assert(not ok and err:is(io.errors.closed))
+	coroutine.nursery(function(go)
+		go(writer.close, writer)
+
+		local buf = string.buffer.new()
+		buf:put("foo")
+		local ok, err = pcall(writer.write, writer, buf)
+		assert(not ok and err:is(io.errors.closed))
+	end)
 end)
 
 t.test("reader:read_to_end() reads until pipe is closed", function()
 	local writer, reader = io.pipe()
-	go(function()
-		local buf = string.buffer.new()
-		buf:put("foo")
-		writer:write(buf)
-		buf:put("bar")
-		writer:write(buf)
-		writer:close()
-	end)
 
-	local content = reader:read_to_end()
-	assert(content == "foobar")
+	coroutine.nursery(function(go)
+		go(function()
+			local buf = string.buffer.new()
+			buf:put("foo")
+			writer:write(buf)
+			buf:put("bar")
+			writer:write(buf)
+			writer:close()
+		end)
+
+		local content = reader:read_to_end()
+		assert(content == "foobar")
+	end)
 end)
--- tests/lua/sync_channel_bench.lua
+++ tests/lua/sync_channel_bench.lua
@@ -2,30 +2,34 @@ local sync = require("sync")
 local t = require("test")
 local time = require("time")
 
-t.bench("1_000_000 send on unbuffered channel", function(b)
+t.bench("send on unbuffered channel", function(b)
 	local tx, rx = sync.channel()
 
-	go(function()
-		for i = 1, b.n do
-			tx:send(i)
+	coroutine.nursery(function(go)
+		go(function()
+			for i = 1, b.n do
+				tx:send(i)
+			end
+		end)
+
+		for _ = 1, b.n do
+			rx:recv()
 		end
 	end)
-
-	for _ = 1, b.n do
-		rx:recv()
-	end
 end)
 
-t.bench("1_000_000 send on a channel with a 1000 buffer", function(b)
+t.bench("send on a channel with a 1000 buffer", function(b)
 	local tx, rx = sync.channel(1000)
 
-	go(function()
-		for i = 1, b.n do
-			tx:send(i)
+	coroutine.nursery(function(go)
+		go(function()
+			for i = 1, b.n do
+				tx:send(i)
+			end
+		end)
+
+		for _ = 1, b.n do
+			rx:recv()
 		end
 	end)
-
-	for _ = 1, b.n do
-		rx:recv()
-	end
 end)
