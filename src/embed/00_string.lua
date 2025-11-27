4b0ccdc3ca281d1f842b6a72c323bea75c4c3ba9
Alexandre Negrel <alexandre@negrel.dev>
2025-11-27 20:11:31 +0100

    add doc comment for added string functions

--- src/embed/00_string.lua
+++ src/embed/00_string.lua
@@ -4,14 +4,18 @@ string.sub = nil
 string.replace_all = string.gsub
 string.gsub = nil
 
+--- Reports whether the string str begins with prefix.
 function string.has_prefix(str, prefix)
 	return string.slice(str, 1, #prefix) == prefix
 end
 
+--- Reports whether the string str ends with suffix.
 function string.has_suffix(str, suffix)
 	return string.slice(str, -#suffix) == suffix
 end
 
+--- Returns string str withtout the provided leading prefix string. If str
+--- doesn't start with prefix, str is returned unchanged.
 function string.strip_prefix(str, prefix)
 	if string.has_prefix(str, prefix) then
 		return string.slice(str, #prefix + 1)
@@ -19,6 +23,8 @@ function string.strip_prefix(str, prefix)
 	return str
 end
 
+--- Returns string str withtout the provided trailing suffix string. If str
+--- doesn't end with prefix, str is returned unchanged.
 function string.strip_suffix(str, suffix)
 	if string.has_suffix(str, suffix) then
 		return string.slice(str, 1, -#suffix - 1)
