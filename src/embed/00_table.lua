a2512f918bb2c7cc60e4d8dbe496a334f298c9f2
Alexandre Negrel <alexandre@negrel.dev>
2025-12-05 23:49:11 +0100

    add table.collect_kv and table.collect helper functions

--- src/embed/00_table.lua
+++ src/embed/00_table.lua
@@ -16,7 +16,7 @@ local freeze_mt = {
 	__newindex = function(t, k, v) error("frozen table") end
 }
 
---- Returns a new frozen (read-only) table that wraps given table.
+--- Return a new frozen (read-only) table that wraps given table.
 function table.freeze(t)
 	assert(type(t) == "table", "freeze require a table argument")
 	-- Already frozen.
@@ -27,10 +27,24 @@ function table.freeze(t)
 	return setmetatable(frozen, freeze_mt)
 end
 
-
+--- Return whether table is empty or not.
 function table.is_empty(t)
 	for _ in pairs(t) do
 		return false
 	end
 	return true
+end
+
+--- Collect keys and values from given iterator into a table.
+function table.collect_kv(...)
+	local t = {}
+	for k, v in ... do t[k] = v end
+	return t
+end
+
+--- Collect values from given iterator into a table.
+function table.collect(...)
+	local t = {}
+	for v in ... do table.insert(t, v) end
+	return t
 end
