09364702c47e40c12f815ac3218f0519da4f7304
Alexandre Negrel <alexandre@negrel.dev>
2025-12-06 15:03:54 +0100

    table: add frozen tables interning

--- src/embed/00_table.lua
+++ src/embed/00_table.lua
@@ -1,18 +1,25 @@
 local table = table
+table.pairs = pairs
+table.ipairs = ipairs
+table.next = next
+table.clear = require("table.clear")
 
-local frozen_tables = setmetatable({}, { __mode = "k" })
+local frozen2table = setmetatable({}, { __mode = "kv" })
+local table2frozen = setmetatable({}, { __mode = "kv" })
 
 local freeze_mt = {
 	__metatable = function(t)
-		return table.freeze(getmetatable(t))
+		return table.freeze(getmetatable(frozen2table[t]))
 	end,
 	__index = function(t, k)
-		local v = frozen_tables[t][k]
+		local v = frozen2table[t][k]
 		if type(v) == "table" then
 			return table.freeze(v)
 		end
 		return v
 	end,
+	__pairs = function(t, k) return pairs(frozen2table[t]) end,
+	__ipairs = function(t, k) return ipairs(frozen2table[t]) end,
 	__newindex = function(t, k, v) error("frozen table") end
 }
 
@@ -20,11 +27,14 @@ local freeze_mt = {
 function table.freeze(t)
 	assert(type(t) == "table", "freeze require a table argument")
 	-- Already frozen.
-	if frozen_tables[t] then return t end
+	if frozen2table[t] then return t end
+	-- Table already has a frozen instance.
+	if table2frozen[t] then return table2frozen[t] end
 
-	local frozen = {}
-	frozen_tables[frozen] = t
-	return setmetatable(frozen, freeze_mt)
+	local frozen = setmetatable({}, freeze_mt)
+	frozen2table[frozen] = t
+	table2frozen[t] = frozen
+	return frozen
 end
 
 --- Return whether table is empty or not.
