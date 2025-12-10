1de842cba12221873da1f4a46a8f032b93c1b8ba
Alexandre Negrel <alexandre@negrel.dev>
2025-12-10 22:10:58 +0100

    add lua hello world

--- cmd/allelua/run.c
+++ cmd/allelua/run.c
@@ -1,13 +1,29 @@
-#include <stdio.h>
+#include <lauxlib.h>
+#include <lua.h>
+#include <lualib.h>
 #include <stdlib.h>
 
 #include "cmd/allelua/main.h"
 
 int run(int argc, char **argv)
 {
+	int err = EXIT_SUCCESS;
+	lua_State *L = luaL_newstate();
+	luaL_openlibs(L);
+
 	(void)argc;
 	(void)argv;
-	printf("running...");
 
-	return EXIT_SUCCESS;
+	const char *lua_script = "print('Hello world!')";
+
+	if (luaL_dostring(L, lua_script)) {
+		fprintf(stderr, "Error: %s\n", lua_tostring(L, -1));
+		lua_pop(L, 1);
+		err = EXIT_FAILURE;
+		goto end;
+	}
+
+end:
+	lua_close(L);
+	return err;
 }
--- flake.nix
+++ flake.nix
@@ -7,6 +7,7 @@
 
   outputs =
     {
+      self,
       nixpkgs,
       flake-utils,
       eventloop,
@@ -20,16 +21,24 @@
           pkgs = import nixpkgs {
             inherit system;
           };
+          lib = pkgs.lib;
         in
         {
           devShells = {
-            default = pkgs.mkShell {
-              buildInputs = with pkgs; [
-                clang-tools
-                valgrind
-              ];
+            default = pkgs.mkShell rec {
+              buildInputs =
+                with pkgs;
+                [
+                  clang-tools
+                  valgrind
+                  pkg-config
+                ]
+                ++ [ self.packages.${system}.luajit ];
 
               EVENTLOOP_INCLUDE = "${eventloop.packages.${system}.default}/include";
+              LUAJIT_INCLUDE = "${self.packages.${system}.luajit}/include";
+              LUAJIT_LIB = "${self.packages.${system}.luajit}/lib";
+              LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
             };
           };
           packages = {
