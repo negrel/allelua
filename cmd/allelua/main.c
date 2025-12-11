0dc667d8a5cc03e2796a39d38e16398672942f81
Alexandre Negrel <alexandre@negrel.dev>
2025-12-11 11:50:15 +0100

    fix vfprintf warning

--- cmd/allelua/main.c
+++ cmd/allelua/main.c
@@ -15,14 +15,25 @@ void usage(FILE *out);
 /**
  * Print CLI error and exit.
  */
-void cli_error(FILE *out, char *fmt, ...);
+#define CLI_ERRORF(out, fmt, ...)                                              \
+	do {                                                                   \
+		fprintf(out, "Error: ");                                       \
+		fprintf(out, fmt, __VA_ARGS__);                                \
+		fprintf(out, "\n\n");                                          \
+		fprintf(out, "USAGE: allelua COMMAND [ARGS...]\n");            \
+		fprintf(out, "\n");                                            \
+		fprintf(out, "Run 'allelua -h' for more informations\n");      \
+		fflush(out);                                                   \
+		exit(EXIT_FAILURE);                                            \
+	} while (0)
+#define CLI_ERROR(out, msg) CLI_ERRORF(out, "%s", msg)
 
 int main(int argc, char **argv)
 {
 	int option;
 
 	if (argc < 2)
-		cli_error(stderr, "no command provided");
+		CLI_ERROR(stderr, "no command provided");
 
 	char *cmd = argv[1];
 	if (strcmp(cmd, "run") == 0) {
@@ -43,7 +54,7 @@ int main(int argc, char **argv)
 		}
 	}
 
-	cli_error(stderr, "unknown command '%s'", cmd);
+	CLI_ERRORF(stderr, "unknown command '%s'", cmd);
 	return EXIT_SUCCESS;
 }
 
@@ -65,22 +76,4 @@ void usage(FILE *out)
 	fprintf(out, "\n");
 	fprintf(out, "Source code: https://github.com/negrel/allelua\n");
 	fflush(out);
-}
-
-void cli_error(FILE *out, char *fmt, ...)
-{
-	va_list args;
-	va_start(args, fmt);
-
-	fprintf(out, "Error: ");
-	vfprintf(out, fmt, args);
-	fprintf(out, "\n\n");
-	fprintf(out, "USAGE: allelua COMMAND [ARGS...]\n");
-	fprintf(out, "\n");
-	fprintf(out, "Run 'allelua -h' for more informations\n");
-	fflush(out);
-
-	va_end(args);
-
-	exit(EXIT_FAILURE);
 }
